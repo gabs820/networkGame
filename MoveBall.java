fb8cdd0baf80083fc9f9d0d6cefbb655d0fe8bdf
Shae Trimmer <trimm103@mail.chapman.edu>
2017-04-14 07:27:53 -0700

    adds new file MoveBall.java

---
+++ MoveBall.java
@@ -0,0 +1,16 @@
+// Edit from Source: https://www.youtube.com/watch?v=p9Y-NBg8eto
+
+import javax.swing.JFrame;
+
+public class MoveBall {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		MyKeyListener listener = new MyKeyListener();
+		
+		frame.add(listener);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,600);
+	}
+}
