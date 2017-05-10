f93777843ac9cfcd5e6a7a0d36ceef187cf98523
Shae Trimmer <trimm103@mail.chapman.edu>
2017-05-09 21:25:09 -0700

    modified file MassChaosClient.java to no longer require user send message when starting

--- MassChaosClient.java
+++ MassChaosClient.java
@@ -7,12 +7,13 @@ public class MassChaosClient
 
 	public static void main (String args[]) throws Exception 
 	{
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 			DatagramSocket clientSocket = new DatagramSocket();
 			InetAddress IPAddress = InetAddress.getByName("localhost");
 			byte[] sendData = new byte[1024];
 			byte[] receiveData = new byte[1024];
-		  String sentence = inFromUser.readLine();
+		  //String sentence = inFromUser.readLine();
+		  String sentence = "Connection established!";
 		  sendData = sentence.getBytes();
 		  DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 		  clientSocket.send(sendPacket);
