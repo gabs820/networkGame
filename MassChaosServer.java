6927571cec0e95086c3b478b029b51186d81cecc
Gabby Llanillo <llani100@mail.chapman.edu>
2017-05-10 01:42:00 -0700

    removed comments

--- MassChaosServer.java
+++ MassChaosServer.java
@@ -87,6 +87,7 @@ public class MassChaosServer {
 				serverSocket.receive(receivePacket);
 				String sentence = new String(receivePacket.getData());
 				
+				//if isGameOver is true then BYE is sent and server socket is closed
 				if (sentence.trim().equals("BYE")) {
 					serverSocket.close();
 					break;
@@ -110,87 +111,7 @@ public class MassChaosServer {
 				
 			} 
 		}
-		//serverSocket.close();
 		
 	}
-		
 }
 	
-	/*// Maintain list of all client sockets for broadcast
-	private ArrayList<Socket> socketList;
-
-	public MassChaosServer()
-	{
-		socketList = new ArrayList<Socket>();
-	}
-
-	private void getConnection()
-	{
-		// Wait for a connection from the client
-		try
-		{
-			System.out.println("Waiting for client connections on port 7654.");
-			ServerSocket serverSock = new ServerSocket(7654);
-			// This is an infinite loop, the user will have to shut it down
-			// using control-c
-			while (true)
-			{
-				Socket connectionSock = serverSock.accept();
-				// Add this socket to the list
-				socketList.add(connectionSock);
-				// Send to ClientHandler the socket and arraylist of all sockets
-				MassChaosClientHandler handler = new MassChaosClientHandler(connectionSock, this.socketList);
-				Thread theThread = new Thread(handler);
-				theThread.start();
-			}
-			// Will never get here, but if the above loop is given
-			// an exit condition then we'll go ahead and close the socket
-			//serverSock.close();
-		}
-		catch (IOException e)
-		{
-			System.out.println(e.getMessage());
-		}
-	}
-
-	public static void main(String[] args)
-	{
-		MassChaosServer server = new MassChaosServer();
-		server.getConnection();
-	}
-	
-	private DatagramSocket socket;
-	private Game game;
-			
-	public MassChaosServer(Game game) {
-		this.game = game;
-		try {
-			this.socket = new DatagramSocket(7654);
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-	
-	}
-	
-	public void run() {
-        while (true) {
-            byte[] data = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(data, data.length);
-            try {
-                socket.receive(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            this.parsePacket(packet.getData(), packet.getAddress(), packet.getPort());
-        }
-    }
-	
-	public void sendData(byte[] data, InetAddress ipAddress, int port) {
-		DatagramPacket packet = new DatagramPacket(data, data.length, ipAddress, port);
-		try {
-			this.socket.send(packet);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}*/
-
